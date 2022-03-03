4a9af77c40602d7ce4e98eed9da6902b7ee507ba
MELF晓宇 <xyzh.melf@petalmail.com>
2022-03-03 23:17:04 +0800

    更新readme文件、更新依赖版本、重写跨域中间件、封装类型转换函数

--- utils/data/change_format.go
+++ utils/data/change_format.go
@@ -46,38 +46,135 @@ func ToString(i interface{}) (str string) {
 			ba = append(ba, byte(b))
 		}
 		str = string(ba)
+	case error:
+		str = i.(error).Error()
 	default:
-		panic("该类型不支持")
+		panic("该类型暂不支持")
 	}
 	return
 }
 
+func ToInt(i interface{}) (num int, err error) {
+	switch i.(type) {
+	case string:
+		num, err = strconv.Atoi(i.(string))
+	}
+	return
+}
+
+// ToInt32
+/**
+ * @Description: 将任意类型转为int32类型
+ * @param i
+ * @return num
+ * @return err
+ */
 func ToInt32(i interface{}) (num int32, err error) {
 	switch i.(type) {
+	case int:
+		num = int32(i.(int))
+	case int32:
+		num = i.(int32)
+	case int64:
+		// 有可能造成精度丢失
+		num = int32(i.(int64))
+	case float32:
+		// 有可能造成精度丢失
+		num = int32(i.(float32))
+	case float64:
+		// 有可能造成精度丢失
+		num = int32(i.(float64))
 	case string:
 		n, e := strconv.Atoi(i.(string))
 		num = int32(n)
 		err = e
+	default:
+		panic("该类型暂不支持")
 	}
 	return
 }
 
+// ToInt64
+/**
+ * @Description: 将任意类型转为int64类型
+ * @param i
+ * @return num
+ * @return err
+ */
 func ToInt64(i interface{}) (num int64, err error) {
 	switch i.(type) {
+	case int:
+		num = int64(i.(int))
 	case int32:
 		num = int64(i.(int32))
 	case int64:
 		num = i.(int64)
+	case float32:
+		num = int64(i.(float32))
+	case float64:
+		num = int64(i.(float64))
 	case string:
 		num, err = strconv.ParseInt(i.(string), 10, 64)
+	default:
+		panic("该类型暂不支持")
 	}
 	return
 }
 
+// ToFloat32
+/**
+ * @Description: 将任意类型转为float32类型
+ * @param i
+ * @return num
+ * @return err
+ */
 func ToFloat32(i interface{}) (num float32, err error) {
+	switch i.(type) {
+	case string:
+		// string无法直接转换float32，只能先转换为float64，再通过float64转float32
+		var num64 float64
+		num64, err = strconv.ParseFloat(i.(string), 32)
+		num = float32(num64)
+	case int:
+		num = float32(i.(int))
+	case int32:
+		num = float32(i.(int32))
+	case int64:
+		num = float32(i.(int64))
+	case float32:
+		num = i.(float32)
+	case float64:
+		// 可能造成精度丢失
+		num = float32(i.(float64))
+	default:
+		panic("该类型暂不支持")
+	}
 	return
 }
 
+// ToFloat64
+/**
+ * @Description: 将任意类型转为float64类型
+ * @param i
+ * @return num
+ * @return err
+ */
 func ToFloat64(i interface{}) (num float64, err error) {
+	switch i.(type) {
+	case string:
+		num, err = strconv.ParseFloat(i.(string), 64)
+	case int:
+		num = float64(i.(int))
+	case int32:
+		num = float64(i.(int32))
+	case int64:
+		num = float64(i.(int64))
+	case float32:
+		num = float64(i.(float32))
+	case float64:
+		num = i.(float64)
+	default:
+		panic("该类型暂不支持")
+	}
 	return
 }
